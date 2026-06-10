59bd35d5b6f6442291affbfd05dfb3d2e2e577bc
Paweł Młynarz <38491170+pawelmlynarz@users.noreply.github.com>
2026-06-10 22:37:29 +0200

    version info (#33)

---
+++ engine/source/runtime/core/public/version.h
@@ -0,0 +1,23 @@
+// © 2026 Pawel Mlynarz
+
+#pragma once
+
+#include "tools/compiler.h"
+#include "common/types.h"
+
+#define PX_VERSION_MAJOR 0
+#define PX_VERSION_MINOR 0
+#define PX_VERSION_PATCH 0
+
+#define PX_VERSION_STRING \
+PX_STRINGIZE(PX_VERSION_MAJOR) "." \
+PX_STRINGIZE(PX_VERSION_MINOR) "." \
+PX_STRINGIZE(PX_VERSION_PATCH)
+
+namespace px {
+
+constexpr std::string_view VersionString() {
+    return PX_VERSION_STRING;
+}
+
+} // namespace px
