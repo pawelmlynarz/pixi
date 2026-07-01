ef161e0f0c5967c461f3f4f38eb4ff3657b5f043
Paweł Młynarz <38491170+pawelmlynarz@users.noreply.github.com>
2026-07-01 22:24:53 +0200

    clang tidy uses presets (#52)

--- engine/tools/cmake/platform/toolchains/t-x64-clang.cmake
+++ engine/tools/cmake/platform/toolchains/t-x64-clang.cmake
@@ -1,26 +1,13 @@
 # t-x64-clang.cmake
 
-set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-# Use clang-cl (NOT gcc-style clang)
-set(CMAKE_C_COMPILER clang-cl)
-set(CMAKE_CXX_COMPILER clang-cl)
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
 
-# Ensure MSVC compatibility mode
-set(CMAKE_C_COMPILER_FRONTEND_VARIANT MSVC)
-set(CMAKE_CXX_COMPILER_FRONTEND_VARIANT MSVC)
+set(CMAKE_LINKER lld)
+set(CMAKE_AR llvm-ar)
 
-# Enable MSVC-like behavior
-set(CMAKE_C_STANDARD_COMPUTED_DEFAULT "17")
-set(CMAKE_CXX_STANDARD_COMPUTED_DEFAULT "23")
-
-# Enforce runtime selection consistency (MD/MDd)
-set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
-
-set(CMAKE_LINKER lld-link)
-set(CMAKE_AR llvm-lib)
-set(CMAKE_RC_COMPILER llvm-rc)
-
-set(CMAKE_C_COMPILER_ID Clang)
-set(CMAKE_CXX_COMPILER_ID Clang)
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
--- engine/tools/python/clang_tidy_runner.py
+++ engine/tools/python/clang_tidy_runner.py
@@ -1,42 +1,37 @@
 from auxiliary import *
 from commands import CMakeCommand
 
+
 class ClangTidyRunner:
+    PRESET_NAME = 'ninja-clang-cl'
     TOOL_NAME = 'clang-tidy'
-    GENERATOR = 'Ninja'
-    C_COMPILER = 'clang'
-    CXX_COMPILER = 'clang++'
+
     BUILD_DIR = os.path.join(OUTPUT_DIR, 'build_clang_tidy/')
     SCRIPT_FILE_PATH = os.path.join(CMAKE_DIR, 'run_clang_tidy.cmake')
 
     def __init__(self):
         is_tool_available(ClangTidyRunner.TOOL_NAME)
-        is_tool_available(ClangTidyRunner.GENERATOR)
-        is_tool_available(ClangTidyRunner.C_COMPILER)
-        is_tool_available(ClangTidyRunner.CXX_COMPILER)
-        
-        self.configure_command = CMakeCommand()\
-                                 .append(f"-S .")\
-                                 .append(f"-B {ClangTidyRunner.BUILD_DIR}")\
-                                 .append(f"-G {ClangTidyRunner.GENERATOR}")\
-                                 .append(f"-DCMAKE_C_COMPILER={ClangTidyRunner.C_COMPILER}")\
-                                 .append(f"-DCMAKE_CXX_COMPILER={ClangTidyRunner.CXX_COMPILER}")\
-                                 .append(f"-DCMAKE_EXPORT_COMPILE_COMMANDS=ON")\
-                                 .append(f"-DBUILD_CLANG_TIDY=ON")
 
-        self.script_command = CMakeCommand()\
-                              .append(f"-DPROJECT_BINARY_DIR={ClangTidyRunner.BUILD_DIR}")\
-                              .append(f"-P {ClangTidyRunner.SCRIPT_FILE_PATH}")
+        self.configure_command = CMakeCommand() \
+            .append(f"--preset {ClangTidyRunner.PRESET_NAME}") \
+            .append(f"-B {ClangTidyRunner.BUILD_DIR}") \
+            .append(f"-DCMAKE_EXPORT_COMPILE_COMMANDS=ON") \
+            .append(f"-DCMAKE_BUILD_TYPE=DebugEditor") \
+            .append(f"-DBUILD_CLANG_TIDY=ON") 
+
+        self.script_command = CMakeCommand() \
+            .append(f"-DPROJECT_BINARY_DIR={ClangTidyRunner.BUILD_DIR}") \
+            .append(f"-P {ClangTidyRunner.SCRIPT_FILE_PATH}")
 
     @staticmethod
     def _prepare_compile_commands_file():
         compile_commands_filepath = os.path.join(ClangTidyRunner.BUILD_DIR, 'compile_commands.json')
-        
+
         with open(compile_commands_filepath, 'r') as file:
             data = file.read()
 
         from re import sub
-        modified_data = sub(r"(-I)([^ ]*third_party[/\\][^ ]*)", r"-isystem \2", data)
+        modified_data = sub(r"(-I)([^ ]*third_party[/\\][^ ]*)", r"/imsvc \2", data)
 
         with open(compile_commands_filepath, 'w') as file:
             file.write(modified_data)
