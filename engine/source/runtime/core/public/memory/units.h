5d322f5bdb009073f52726aa1fda62785dbdcad8
Paweł Młynarz <38491170+pawelmlynarz@users.noreply.github.com>
2026-08-17 22:40:51 +0200

    update to vs2026 (#57)

--- engine/source/runtime/core/public/memory/units.h
+++ engine/source/runtime/core/public/memory/units.h
@@ -2,18 +2,18 @@
 
 #pragma once
 
-constexpr std::size_t operator"" _B(unsigned long long const n) {
+constexpr std::size_t operator""_B(unsigned long long const n) {
     return n;
 }
 
-constexpr std::size_t operator"" _KiB(unsigned long long const n) {
+constexpr std::size_t operator""_KiB(unsigned long long const n) {
     return n * 1024ull;
 }
 
-constexpr std::size_t operator"" _MiB(unsigned long long const n) {
+constexpr std::size_t operator""_MiB(unsigned long long const n) {
     return n * 1024ull * 1024ull;
 }
 
-constexpr std::size_t operator"" _GiB(unsigned long long const n) {
+constexpr std::size_t operator""_GiB(unsigned long long const n) {
     return n * 1024ull * 1024ull * 1024ull;
 }
